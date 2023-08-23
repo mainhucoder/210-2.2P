e115a682aa31f98953eb0e146cceb088dd25e913
mainhucoder <133193921+mainhucoder@users.noreply.github.com>
2023-08-23 18:26:00 +0530

    Update 2.1P.ino

--- 2.1P.ino
+++ 2.1P.ino
@@ -1,36 +1,34 @@
-const int ledPin = LED_BUILTIN; // Built-in LED pin
-const int buttonPin = 2;       // Push button pin
+const int ledPin = LED_BUILTIN;   // Pin for the built-in LED
+const int buttonPin = 2;         // Pin for the push button
 
-const long dotDuration = 250;  // Duration of a dot in milliseconds
+const long dotDuration = 250;    // Duration of a dot in milliseconds
 const long dashDuration = dotDuration * 3; // Duration of a dash
 
 const char morseCode[][4] = {
-  "..-",   // U
-  "-..",   // D
-  ".-",    // A
-  "-.--"   // Y
+  "..-",   // U (U stands for "..-" in Morse code)
+  "-..",   // D (D stands for "-.." in Morse code)
+  ".-",    // A (A stands for ".-" in Morse code)
+  "-.--"   // Y (Y stands for "-.--" in Morse code)
 };
 
-
-int currentLetter = 0;
-bool blinking = false;
+int currentLetter = 0;    // Index of the current letter being blinked
+bool blinking = false;    // Flag to control blinking state
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);      // Set LED pin as output
+  pinMode(buttonPin, INPUT_PULLUP); // Set button pin as input with pull-up resistor
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == LOW) { // Button pressed
+  if (digitalRead(buttonPin) == LOW) { // Check if button is pressed
     if (!blinking) {
-      blinking = true;
+      blinking = true;       // Start blinking if not already blinking
       blinkName();
-    }
-    else {
-      blinking = false;
+    } else {
+      blinking = false;      // Stop blinking if already blinking
       digitalWrite(ledPin, LOW); // Turn off LED
     }
-    delay(300); // Debounce delay
+    delay(300); // Debounce delay to prevent rapid toggling
   }
 }
 
@@ -38,21 +36,21 @@ void blinkName() {
   while (blinking) {
     char letter = morseCode[currentLetter][0];
     if (letter == '\0') {
-      currentLetter = 0;
-      delay(1000); // Pause between repetitions
+      currentLetter = 0;     // Reset to the first letter after completing the name
+      delay(1000);           // Pause between repetitions
     } else {
       for (int i = 1; morseCode[currentLetter][i] != '\0'; i++) {
-        digitalWrite(ledPin, HIGH); // Turn on LED
+        digitalWrite(ledPin, HIGH); // Turn on LED for dot/dash
         if (morseCode[currentLetter][i] == '.') {
           delay(dotDuration);
         } else if (morseCode[currentLetter][i] == '-') {
           delay(dashDuration);
         }
-        digitalWrite(ledPin, LOW); // Turn off LED
-        delay(dotDuration); // Inter-element gap
+        digitalWrite(ledPin, LOW);  // Turn off LED after dot/dash
+        delay(dotDuration);          // Gap between dots/dashes
       }
-      delay(dashDuration); // Inter-letter gap
-      currentLetter++;
+      delay(dashDuration);           // Gap between letters
+      currentLetter++;               // Move to the next letter
     }
   }
 }
